f0bedffd0f414a69e2bd53d7ee0caeab4f393c1d
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-05-24 19:50:00 +1000

    Random id generator

--- script.js
+++ script.js
@@ -123,3 +123,14 @@ const switchTask = (id, type) => {
   });
   display();
 };
+const randomIdGenerator = () => {
+  const idLength = 6;
+  const str = "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM1234567890";
+
+  let id = "";
+  for (let i = 0; i < idLength; i++) {
+    const randomPosition = Math.floor(Math.random() * str.length);
+    id += str[randomPosition];
+  }
+  return id;
+};
